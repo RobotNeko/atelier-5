b6dfc6a21a9a73dca7999e378dca67b6ead96f0b
RobotNeko <73191153+RobotNeko@users.noreply.github.com>
2020-10-20 14:24:12 -0400

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,11 @@
+basic.forever(function () {
+    basic.showIcon(IconNames.Happy)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(500)
+})
